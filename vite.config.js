b87d3937423022e79d2a57bae29e1d989ddb88a5
Deian <bolduradeian@gmail.com>
2022-05-29 23:01:46 +0300

    proiect

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// eslint-disable-next-line no-undef
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: "/vue_router_final/",
   plugins: [vue()],
   resolve: {
     alias: {
